46872201cb2ded8e97006a2b09b6ca804687099e
Loi001 <mikhail.e.terzi@gmail.com>
2026-06-22 17:52:42 +0000

    add server part, fixed logger

--- main.cpp
+++ main.cpp
@@ -14,6 +14,7 @@
 #include <spdlog/spdlog.h> 
 #include <spdlog/sinks/stdout_color_sinks.h> 
 #include <spdlog/sinks/basic_file_sink.h> 
+#include <httplib.h>
 
 using json = nlohmann::json;
 
@@ -23,53 +24,55 @@ const std::unordered_set<std::string> image_extensions = { ".jpg", ".jpeg", ".pn
 
 enum log_type { CONSOLE, TXTFILE, ALL };
 
-void init_log(log_type type)
+std::string json_data = "{}";
+std::mutex json_mutex;
+
+void init_log(log_type type, spdlog::level::level_enum level)
 {
 	std::vector<spdlog::sink_ptr> sinks;
 	if (type == CONSOLE || type == ALL) {
 		auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+		console_sink->set_level(level);
 		sinks.push_back(console_sink);
 	}
 	if(type == TXTFILE || type == ALL) {
+		std::filesystem::create_directories("logs");
 		auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/log.txt", true);
+		file_sink->set_level(level);
 		sinks.push_back(file_sink);
 	}
 	if (sinks.empty()) {
-		std::cerr << "No valid log sinks created, falling back to console" << std::endl;
+		spdlog::error("No valid log sinks created, falling back to console");
 		sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
 	}
 	auto logger = std::make_shared<spdlog::logger>("multi_sink", sinks.begin(), sinks.end());
 	spdlog::set_default_logger(logger);
+	logger->set_level(level);
 }
-
-
 
 bool is_valid_directory_path(const std::string& path)
 {
-	if(path.empty()) return false;
-	std::error_code err_code;
-	std::filesystem::path directory_path(path);
-	auto status = std::filesystem::status(directory_path, err_code);
-	if(!err_code){
-		if(!std::filesystem::is_directory(directory_path)) return false;
-		auto premissions = status.permissions();
-		if((premissions & std::filesystem::perms::owner_write) != std::filesystem::perms::none) 
-		{
-			return true;
-		}
+	if (path.empty()) return false;
+
+	std::error_code ec;
+	std::filesystem::path fs_path(path);
+	auto status = std::filesystem::status(fs_path, ec);
+
+	if (!ec && std::filesystem::is_directory(status)) {
+		auto perms = status.permissions();
+		return (perms & std::filesystem::perms::owner_write) != std::filesystem::perms::none;
 	}
-	std::filesystem::path parent = directory_path.parent_path();
-	if (parent.empty()) {
-		parent = std::filesystem::current_path();
-	}
-	std::error_code parent_err_code;
-	auto parent_status = std::filesystem::status(parent, parent_err_code);
-	if(parent_err_code || !std::filesystem::is_directory(parent_status)){
-		return false;
-	}
-	auto parent_premissons = parent_status.permissions();
-	if((parent_premissons & std::filesystem::perms::owner_write) != std::filesystem::perms::none) return true;
-	return true;
+
+	std::filesystem::path parent = fs_path.parent_path();
+	if (parent.empty()) parent = std::filesystem::current_path();
+
+	std::error_code parent_ec;
+	auto parent_status = std::filesystem::status(parent, parent_ec);
+
+	if (parent_ec || !std::filesystem::is_directory(parent_status)) return false;
+
+	auto parent_perms = parent_status.permissions();
+	return (parent_perms & std::filesystem::perms::owner_write) != std::filesystem::perms::none;
 }
 
 TEST_CASE("is_valid_dir_test1") {
@@ -82,22 +85,26 @@ TEST_CASE("is_valid_dir_test2") {
 }
 
 TEST_CASE("is_valid_dir_test3") {
-    CHECK(is_valid_directory_path("./im_does_not_exists") == true);
-}
-
-TEST_CASE("is_valid_dir_test4") {
     CHECK(is_valid_directory_path("") == false);
 }
 
-bool check_directory(std::filesystem::path dir_path, std::filesystem::path output_path) {
+void set_json_data(const std::string& value) {
+	spdlog::debug("Set new json data");
+	std::lock_guard<std::mutex> lock(json_mutex);
+	json_data = value;
+}
+
+std::string get_json_data() {
+	std::lock_guard<std::mutex> lock(json_mutex);
+	return json_data;
+}
+
+bool check_directory(std::filesystem::path dir_path) {
 	try {
 		json media_files;
 		media_files["audio_files"] = json::array();
 		media_files["video_files"] = json::array();
 		media_files["image_files"] = json::array();
-
-		std::filesystem::path media_dir = output_path / "media_files";
-		std::filesystem::create_directories(media_dir);
 
 		for (const auto& entry : std::filesystem::directory_iterator(dir_path)) {
 			if (!entry.is_regular_file()) continue;
@@ -105,39 +112,41 @@ bool check_directory(std::filesystem::path dir_path, std::filesystem::path outpu
 			std::transform(extension.begin(), extension.end(), extension.begin(), [](unsigned char c) { return std::tolower(c); });
 
 			if (audio_extensions.count(extension)) {
-				spdlog::debug("find new file: " + entry.path().string());
+				spdlog::debug("find new file: {}", entry.path().string());
 				media_files["audio_files"].push_back(entry.path().filename().string());
 			}
 			else if (video_extensions.count(extension)) {
-				spdlog::debug("find new file: " + entry.path().string());
+				spdlog::debug("find new file: {}", entry.path().string());
 				media_files["video_files"].push_back(entry.path().filename().string());
 			}
 			else if (image_extensions.count(extension)) {
-				spdlog::debug("find new file: " + entry.path().string());
+				spdlog::debug("find new file: {}", entry.path().string());
 				media_files["image_files"].push_back(entry.path().filename().string());
 			}
 		}
-		std::filesystem::path json_path = media_dir / "media_list.json";
-		spdlog::debug("output path: " + json_path.string());
-		std::ofstream output_file(json_path);
-		if (output_file.is_open()) {
-			output_file << media_files.dump(4);
-			output_file.close();
-		}
-		else {
-			std::cout << "Error: open json file" << std::endl;
-			return false;
-		}
+		spdlog::debug("Dumping file to string...\n");
+		set_json_data(media_files.dump(4));
 	}
 	catch (std::filesystem::filesystem_error& e) {
-		std::cout << "Error: " << e.what() << std::endl;
+		spdlog::error(e.what());
 	}
 	return true;
 }
 
+void start_server()
+{
+	spdlog::debug("Start server");
+	httplib::Server server;
+	server.Get("/media_files", [](const httplib::Request& request, httplib::Response& response){
+		response.set_content(get_json_data(), "application/json");
+	});
+	spdlog::debug("HTTP server running on http://localhost:1234/media_files");
+	server.listen("localhost", 1234);
+}
+
 int main(int argc, char** argv) {
 	spdlog::level::level_enum log_level = spdlog::level::info;
-	log_type log_type = ALL;
+	log_type log_output_type = ALL;
 	
 	// tests
 	doctest::Context context;
@@ -152,8 +161,6 @@ int main(int argc, char** argv) {
 				std::cout << "Test result: " << test_result << std::endl;
 				return test_result;
 			}
-		std::cout << "Test result: " << test_result << std::endl;
-			break;
 		}
 		if(arg.find("--log-level=") == 0) {
 			std::string level_str = arg.substr(12);
@@ -167,21 +174,15 @@ int main(int argc, char** argv) {
 		}
 		if(arg.find("--log-type=") == 0) {
 			std::string type_str = arg.substr(11);
-			if(type_str == "console") log_type = CONSOLE;
-			else if(type_str == "txtfile") log_type = TXTFILE;
-			else if(type_str == "all") log_type = ALL;
+			if(type_str == "console") log_output_type = CONSOLE;
+			else if(type_str == "txtfile") log_output_type = TXTFILE;
+			else if(type_str == "all") log_output_type = ALL;
 		}
 	}
 
 	// init logs
-	init_log(log_type);
-	spdlog::set_level(log_level);
-
-	// test log
-	// spdlog::info("Log initialized with level: {} and type: {}", spdlog::level::to_string_view(log_level), log_type == CONSOLE ? "console" : log_type == TXTFILE ? "txtfile" : "all");
-	// spdlog::debug("Debug log message");
-	// spdlog::warn("Warning log message");
-	// spdlog::error("Error log message");	
+	init_log(log_output_type, log_level);
+	spdlog::flush_on(spdlog::level::debug);
 
 	// main logic
 	std::string home_directory, output_directory;
@@ -190,14 +191,7 @@ int main(int argc, char** argv) {
 	std::cout << "Enter home directory: ";
 	std::cin >> home_directory;
 	if (!std::filesystem::is_directory(home_directory)) {
-		std::cout << "Error: directory does not exist or it's not a directory\n";
-		return 0;
-	}
-
-	std::cout << "Enter output directory: ";
-	std::cin >> output_directory;
-	if (!is_valid_directory_path(output_directory)) {
-		std::cout << "Error: invalid directory name\n";
+		spdlog::critical("directory does not exist or it's not a directory");
 		return 0;
 	}
 
@@ -205,21 +199,25 @@ int main(int argc, char** argv) {
 	while (!(std::cin >> delay)) {
 		std::cin.clear();
 		std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-		std::cout << "Error: invalid input type\n";
+		spdlog::error("invalid input type");
 	}
 	if (delay <= 0) {
-		std::cout << "Error: invalid delay value\n";
+		spdlog::critical("invalid delay value\n");
 		return 0;
 	}
 
 	std::filesystem::path home(home_directory);
-	std::filesystem::path out(output_directory);
+
+	std::thread server_thread(start_server);
+	server_thread.detach();
+	std::cout << "Server started at: " << "http://localhost:1234/media_files" << std::endl;
 
 	while (true)
 	{
-		if (!check_directory(home, out)) { return 0; }
+		if (!check_directory(home)) { break; }
 		std::this_thread::sleep_for(std::chrono::milliseconds(delay));
 	}
 
+	spdlog::shutdown();
 	return 0;
 }
